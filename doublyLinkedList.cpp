96cfbbf97362b3ce762ebb1c4627073f6cca8557
Bartosz Dobrzycki <bartosz.dobrzycki@gmail.com>
2014-02-02 15:50:55 +0000

    Made m_tail usable. Added tests.

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -8,7 +8,6 @@
 #include <stdlib.h>
 #include <crtdbg.h>
 
-#include <cmath>
 #include <iostream>
 
 // These objects will be stored on the doubly linked list.
@@ -18,15 +17,15 @@ private:
     int m_data;
 
 public:
-    explicit Foo ( int data )
-        :m_data( data )
+    explicit Foo( int data )
+        : m_data( data )
     {
 
     }
 
     ~Foo()
     {
-
+        std::cout << "~Foo() " << m_data << std::endl;
     }
 
     inline void setData( int newData ) { m_data = newData; }
@@ -46,13 +45,14 @@ public:
         T* const  m_key;
 
         explicit Node( T* key )
-            :m_next( nullptr ), m_prev( nullptr ), m_key( key )
+            : m_next( nullptr ), m_prev( nullptr ), m_key( key )
         {
 
         }
 
         ~Node()
         {
+            std::cout << "~Node()" << std::endl;
             delete m_key;
         }
     };
@@ -65,17 +65,24 @@ public:
 
     ~DoublyLinkedList()
     {
+        deallocate();
+    }
+
+    void deallocate( void )
+    {
         Node* node = getHead();
         while( node != nullptr )
         {
-            deleteNode( node );
             Node* nodeNext = node->m_next;
-            delete node;
+            deleteNode( node );
             node = nodeNext;
         }
+        m_head = nullptr;
+        m_tail = nullptr;
     }
 
-    inline Node *const getHead() { return m_head; }
+    inline Node* const getHead() const { return m_head; }
+    inline Node* const getTail() const { return m_tail; }
 
     // Insert
     void insert( T* element )
@@ -86,11 +93,14 @@ public:
         {
             m_head->m_prev = newNode;
         }
+        else
+        {
+            m_tail = newNode;
+        }
         m_head = newNode;
         newNode->m_prev = nullptr;
     }
 
-private:
     void deleteNode( Node* node )
     {
         if( node->m_prev != nullptr )
@@ -106,13 +116,19 @@ private:
         {
             node->m_next->m_prev = node->m_prev;
         }
+        else
+        {
+            m_tail = node->m_prev;
+        }
+
+        delete node;
     }
 
+private:
     Node* m_head;
     Node* m_tail;
 };
 
-// Print
 void print( DoublyLinkedList< Foo > & list )
 {
     DoublyLinkedList< Foo >::Node* node = list.getHead();
@@ -126,17 +142,73 @@ void print( DoublyLinkedList< Foo > & list )
         }
     }
     std::cout << " }" << std::endl;
+
+    if( list.getHead() )
+    {
+        std::cout << "head: " << list.getHead()->m_key->getData() << std::endl;
+    }
+
+    if( list.getTail() )
+    {
+        std::cout << "tail: " << list.getTail()->m_key->getData() << std::endl;
+    }
+}
+
+void deleteElement( DoublyLinkedList< Foo > & list, int data )
+{
+    DoublyLinkedList< Foo >::Node* node = list.getTail();
+    while( node != 0 && node->m_key->getData() != data )
+    {
+        node = node->m_prev;
+    }
+
+    if( node )
+    {
+        std::cout << "deleting: " << node->m_key->getData() << std::endl;
+        list.deleteNode( node );
+    }
+    else
+    {
+        std::cout << data << " not found!" << std::endl;
+    }
 }
 
 int main( int argc, char **argv )
 {
     DoublyLinkedList< Foo > fooList;
-    for( int i = 0; i < 10; i++ )
-    {
-        fooList.insert( new Foo( i ) );
-    }
+    fooList.insert( new Foo( 1 ) );
+    fooList.insert( new Foo( 4 ) );
+    fooList.insert( new Foo( 16 ) );
+    fooList.insert( new Foo( 9 ) );
+    fooList.insert( new Foo( 25 ) );
+
     print( fooList );
-    fooList.~DoublyLinkedList();
-    _CrtDumpMemoryLeaks();
+    deleteElement( fooList, 4 );
+    print( fooList );
+    deleteElement( fooList, 1 );
+    print( fooList );
+    deleteElement( fooList, 25 );
+    print( fooList );
+    deleteElement( fooList, 25 );
+    print( fooList );
+
+    fooList.deallocate();
+
+    fooList.insert( new Foo( 1 ) );
+    print( fooList );
+    deleteElement( fooList, 1 );
+    print( fooList );
+
+    // _CrtDumpMemoryLeaks returns TRUE if a memory leak is found.
+    // Otherwise, the function returns FALSE.
+    if( _CrtDumpMemoryLeaks() )
+    {
+        std::cout << "Warning: memory leak found!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Memory OK" << std::endl;
+    }
+    std::cin.get();
     return 0;
 }
